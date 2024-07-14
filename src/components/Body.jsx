5af2558fe7b7648607f1d6aaa192229e78a498a3
Siva Ganesh <akulasivaganesh1996@gmail.com>
2024-07-14 23:54:11 +0530

    add realtime data from swiggy

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,47 @@
-import { useEffect, useState } from "react";
-import { restaurants } from "../utils/mockData";
-import RestaurantCard from "./RestaurantCard";
-import { waitFor } from "../utils/waitFor";
+import { useEffect, useState, useRef, useCallback } from "react";
+import RestaurantCard, { withTopRatedLabel } from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import {
+  SWIGGY_RESTAURANT_DATA,
+  SWIGGY_RESTAURANT_LIST_UPDATE,
+} from "../utils/constants";
+import { UPDATE_REQ_PAYLOAD } from "../utils/mockData";
 
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  // const observer = useRef();
+  // const lastResCardRef = (node) => {
+  //   if (observer.current) {
+  //     observer.current.disconnect();
+  //   }
+  //   observer.current = new IntersectionObserver(async (entries) => {
+  //     if (entries[0].isIntersecting) {
+  //       const data = await fetch(SWIGGY_RESTAURANT_LIST_UPDATE, {
+  //         method: "POST",
+  //         headers: {
+  //           "content-type": "application/json",
+  //         },
+  //         body: JSON.stringify(UPDATE_REQ_PAYLOAD),
+  //       });
+  //       jsonData = await data.json();
+  //     }
+  //   });
+  //   if (node) {
+  //     observer.current.observe(node);
+  //   }
+  // };
+
   const fetchData = async () => {
-    await waitFor(1000);
+    const data = await fetch(SWIGGY_RESTAURANT_DATA);
+    const jsonData = await data.json();
+    const card = jsonData.data.cards.find(
+      (card) => card.card.card.id === "restaurant_grid_listing"
+    );
+    const { restaurants } = card.card.card.gridElements.infoWithStyle;
     setRestaurantList(restaurants);
     setFilteredRestaurants(restaurants);
   };
@@ -31,8 +61,7 @@ const Body = () => {
   };
 
   const handleReset = () => {
-    setRestaurantList(restaurants);
-    setFilteredRestaurants(restaurants);
+    setFilteredRestaurants(restaurantList);
     setSearchTerm("");
   };
 
@@ -76,7 +105,17 @@ const Body = () => {
           </div>
         </div>
         <div className="res-container">
-          {filteredRestaurants.map((restaurant) => {
+          {filteredRestaurants.map((restaurant, index) => {
+            // return
+            // filteredRestaurants.length === index + 1 ? (
+            //   <Link
+            //     ref={lastResCardRef}
+            //     key={restaurant.info.id}
+            //     to={"/restaurant/" + restaurant.info.id}
+            //   >
+            //     <RestaurantCard restaurant={restaurant} />
+            //   </Link>
+            // ) : (
             return (
               <Link
                 key={restaurant.info.id}
@@ -85,6 +124,7 @@ const Body = () => {
                 <RestaurantCard restaurant={restaurant} />
               </Link>
             );
+            // );
           })}
         </div>
       </div>
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,34 +1,53 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Shimmer from "./Shimmer";
-import { itemCards } from "../utils/mockData";
 import { waitFor } from "../utils/waitFor";
+import { MENU_ITEMS } from "../utils/constants";
 
 const RestaurantMenu = () => {
   const [menuList, setMenuList] = useState(null);
+  const [info, setInfo] = useState(null);
   const { resId } = useParams();
 
   const fetchMenuData = async () => {
     await waitFor(500);
+    const data = await fetch(MENU_ITEMS + resId);
+    const jsonData = await data.json();
+    const infoData = jsonData.data.cards.find((card) =>
+      card?.card?.card?.hasOwnProperty("info")
+    );
+    const { info } = infoData.card.card;
+    const itemCard = jsonData.data.cards.find((card) =>
+      card.hasOwnProperty("groupedCard")
+    );
+    const menuData = itemCard.groupedCard.cardGroupMap.REGULAR.cards.find(
+      (recomended) =>
+        recomended.card.card.type === "CATEGORY_TYPE_RECOMMENDED" ||
+        recomended.card.card.hasOwnProperty("categories")
+    );
+    const { itemCards } = menuData.card.card.hasOwnProperty("categories")
+      ? menuData.card.card.categories[0]
+      : menuData.card.card;
     setMenuList(itemCards);
+    setInfo(info);
   };
 
   useEffect(() => {
     fetchMenuData();
   }, []);
 
-  if (!menuList) {
+  if (!menuList || !info) {
     return <Shimmer />;
   }
 
   return (
     <div className="res-menu">
-      <h1>Name of the restaurant</h1>
-      <p>Additionl sinlge line info</p>
+      <h1>{info.name}</h1>
+      <p>{info.costForTwoMessage + " - " + info.avgRatingString}</p>
       <ul>
         {menuList.map((item) => {
           return (
-            <li>
+            <li key={item.card.info.id}>
               {item.card.info.name +
                 " - " +
                 item.card.info.category +
